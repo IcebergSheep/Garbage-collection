6cd7ae620ec8ad3960f55f798a4d7a80e8130173
Lee Somi <101859625+IcebergSheep@users.noreply.github.com>
2022-10-24 01:29:45 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,25 @@
 // Express, mysql, body-parser 
-// npm install 설치하기
+// npm install
+//$ mkdir nodejs-express-mysql
+//$ cd nodejs-express-mysql
+//$ npm install express mysql body-parser --save
 
+
+//기본 세팅
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//
+app.get("/", (req, res)=>{
+    res.render('index.js');
+});
+
+// 포트넘버 설정
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000.");
+})
